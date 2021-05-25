3b408ea6a7602bce6b9a853482266eca6a66b4c0
Toshiki.Kawazoe <toshiki.kawazoe@sony.com>
2021-05-25 09:35:59 +0900

    Change worktime print 'feature/191852-24'

--- src/print.rs
+++ src/print.rs
@@ -1,12 +1,14 @@
 
 
-pub fn worktime(title: &str, per: &f32, sum: &f32) {
+pub fn worktime(plan_no: &str, per: &f32, sum: &f32, remark: &str) {
   // 一日の労働時間
   let man_day: f32 = 7.75;
-  println!("{title} ({per}): 累計 {sum:2.2} 時間, {day:2.0} 日間と{hour:2.2} 時間",
-            title = title,
+  println!("{plan_no} ({per}%): 累計 {sum:2.2} 時間, {day:2.0} 日間と{hour:2.2} 時間 : {remark}",
+            plan_no = plan_no,
             per = per,
             sum = sum,
             day = sum / man_day,
-            hour = sum % man_day);
+            hour = sum % man_day,
+            remark = remark
+          );
 }
