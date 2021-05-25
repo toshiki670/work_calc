605238c9dcaaa2e53054cd163bb768f12af976c4
Toshiki.Kawazoe <toshiki.kawazoe@sony.com>
2021-05-25 09:25:22 +0900

    Move 191852-24 'feature/191852-24'

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ fn main() {
 
   assert_eq!(sum_hour, portal_sum + gw_sum + service_sum, "計算結果と合計時間が異なる。");
 
-  print::worktime("191852-24 SBNサービス運営 MC運用業務（業託）", &service, &service_sum);
   print::worktime("206174-01 SBN_クラウドポータルv1.24開発     ", &portal, &portal_sum);
   print::worktime("206175-01 SBN_クラウドGW_v1.24開発          ", &gw, &gw_sum);
+  print::worktime("191852-24 SBNサービス運営 MC運用業務（業託）", &service, &service_sum);
 }
