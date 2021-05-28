9e4e819525f111b605d9cb9e8263b4262dbd0655
Toshiki.Kawazoe <toshiki.kawazoe@sony.com>
2021-05-29 02:19:06 +0900

    work_day も入力できるように変更

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,17 @@ mod print;
 
 fn main() {
   println!("Enter man hour:");
-
   input! {
     sum_hour: f32,
   }
+  assert!(140. <= sum_hour, "一人月の労働時間は140時間以上にしてください。");
+
+  println!("Enter work days:");
+  input! {
+    work_days: u8,
+  }
+  assert!(work_days <= 31, "31日以上入力しないでください。");
+
 
   // SBN_クラウドポータルv1.24開発
   let portal: f32 = 0.5;
@@ -23,7 +30,9 @@ fn main() {
 
   assert_eq!(sum_hour, portal_sum + gw_sum + service_sum, "計算結果と合計時間が異なる。");
 
-  print::worktime("191852-24", &service, &service_sum, "SBNサービス運営_2021年05月/MC運用業務（業託）MC運用業務（業託） ※25日までに入力");
-  print::worktime("206174-01", &portal , &portal_sum , "SBN_クラウドポータルv1.24開発");
-  print::worktime("206175-01", &gw     , &gw_sum     , "SBN_クラウドGW_v1.24開発");
+  let printer = print::Printer::new(sum_hour, work_days);
+
+  printer.worktime("191852-24", &service, &service_sum, "SBNサービス運営_2021年05月/MC運用業務（業託）MC運用業務（業託） ※25日までに入力");
+  printer.worktime("206174-01", &portal , &portal_sum , "SBN_クラウドポータルv1.24開発");
+  printer.worktime("206175-01", &gw     , &gw_sum     , "SBN_クラウドGW_v1.24開発");
 }
--- src/print.rs
+++ src/print.rs
@@ -1,14 +1,46 @@
 
+pub struct Printer {
+  work_hours_per_day: f32,
 
-pub fn worktime(plan_no: &str, per: &f32, sum: &f32, remark: &str) {
-  // 一日の労働時間
-  let man_day: f32 = 7.75;
-  println!("{plan_no} ({per}%): 累計 {sum:2.2} 時間, {day:2.0} 日間と{hour:2.2} 時間 : {remark}",
-            plan_no = plan_no,
-            per = per,
-            sum = sum,
-            day = sum / man_day,
-            hour = sum % man_day,
-            remark = remark
-          );
+}
+
+
+impl Printer {
+  pub fn new(sum_hour: f32, work_days: u8) -> Printer {
+    let work_hours_per_day = Printer::calc_work_hours_per_day(sum_hour, work_days);
+    println!("一日の基本労働時間は {}", work_hours_per_day);
+
+    Printer {
+      work_hours_per_day: work_hours_per_day,
+    }
+  }
+
+  pub fn worktime(&self, plan_no: &str, per: &f32, sum: &f32, remark: &str) {
+
+    println!("{plan_no} ({per}%): 累計 {sum:2.2} 時間, {day:2.0} 日間と{hour:2.2} 時間 : {remark}",
+              plan_no = plan_no,
+              per = per,
+              sum = sum,
+              day = sum / self.work_hours_per_day,
+              hour = sum % self.work_hours_per_day,
+              remark = remark
+            );
+  }
+
+  fn calc_work_hours_per_day(sum_hour: f32, work_days: u8) -> f32 {
+    let integer_time: u32 = sum_hour as u32 / work_days as u32;
+    let surplus_time: f32 = sum_hour as f32 % work_days as f32;
+    let quarter_hour: f32 = 1. / 0.25;
+    let add_time: f32 = ((surplus_time * quarter_hour) as u32 / work_days as u32) as f32 / quarter_hour;
+
+    integer_time as f32 + add_time
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  #[test]
+  fn be_valid_calc_work_hours_per_day() {
+    assert_eq!(super::Printer::calc_work_hours_per_day(140., 16), 8.75);
+  }
 }
