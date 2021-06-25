37731dcffe378fe1284b36ed85aacd6eb6191c35
Toshiki.Kawazoe <toshiki.kawazoe@sony.com>
2021-06-26 02:01:49 +0900

    Replace f32 to WorkHour 'feature/work_hour'

--- src/print.rs
+++ src/print.rs
@@ -1,3 +1,4 @@
+use crate::WorkHour;
 
 pub struct Printer {
   work_hours_per_day: f32,
@@ -6,23 +7,23 @@ pub struct Printer {
 
 
 impl Printer {
-  pub fn new(sum_hour: f32, work_days: u8) -> Printer {
-    let work_hours_per_day = Printer::calc_work_hours_per_day(sum_hour, work_days);
-    println!("一日の基本労働時間は {}", work_hours_per_day);
+  pub fn new(sum_hour: WorkHour, work_days: u8) -> Printer {
+    let hw = sum_hour / work_days as f32;
+    println!("一日の基本労働時間: {:.2} 時間", hw.hour());
 
     Printer {
-      work_hours_per_day: work_hours_per_day,
+      work_hours_per_day: hw.hour(),
     }
   }
 
-  pub fn worktime(&self, plan_no: &str, per: &f32, sum: &f32, remark: &str) {
+  pub fn worktime(&self, plan_no: &str, per: &f32, sum: &WorkHour, remark: &str) {
 
     println!("{plan_no} ({per}%): 累計 {sum:2.2} 時間, {day:2.0} 日間と{hour:2.2} 時間 : {remark}",
               plan_no = plan_no,
               per = per,
               sum = sum,
-              day = sum / self.work_hours_per_day,
-              hour = sum % self.work_hours_per_day,
+              day = (sum.hour() / self.work_hours_per_day) as i32,
+              hour = sum.hour() % self.work_hours_per_day,
               remark = remark
             );
   }
