8417cda15cc0c0a0b311ead0bf33bfed7999b49e
Toshiki.Kawazoe <toshiki.kawazoe@sony.com>
2021-08-23 18:24:22 +0900

    Fix indent

--- src/main.rs
+++ src/main.rs
@@ -8,101 +8,114 @@ mod plan;
 mod work_hour;
 use crate::work_hour::WorkHour;
 
-
 fn main() {
-  let cli = clap::load_yaml!("cli.yml");
-  let matches = App::from_yaml(cli).get_matches();
+    let cli = clap::load_yaml!("cli.yml");
+    let matches = App::from_yaml(cli).get_matches();
 
-  if matches.is_present("verbose") {
-    env::set_var("RUST_LOG", Level::Trace.to_string());
-  }
-  env_logger::init();
+    if matches.is_present("verbose") {
+        env::set_var("RUST_LOG", Level::Trace.to_string());
+    }
+    env_logger::init();
 
-  // Total hour
-  let raw_total_hour = matches.value_of("total_hour").unwrap();
-  let total_hour = raw_total_hour.parse();
-  if let Err(e) = total_hour {
-    error!("合計時間が誤っています。");
-    error!("数字であることを確認してください: {}.", &raw_total_hour);
-    panic!("ParseError: {}.", e);
-  }
+    // Total hour
+    let raw_total_hour = matches.value_of("total_hour").unwrap();
+    let total_hour = raw_total_hour.parse();
+    if let Err(e) = total_hour {
+        error!("合計時間が誤っています。");
+        error!("数字であることを確認してください: {}.", &raw_total_hour);
+        panic!("ParseError: {}.", e);
+    }
 
-  let total_hour = WorkHour::new(total_hour.unwrap());
-  debug!("total_hour({}): {:?}", &total_hour, &total_hour);
-  if 140. > total_hour.raw() {
-    error!("一人月の労働時間は140時間以上にしてください。");
-    return;
-  } else if 0. < total_hour.reminder() {
-    error!("{:.2}時間余分です。労働時間は15分刻みで入力してください。", total_hour.reminder());
-    return;
-  }
+    let total_hour = WorkHour::new(total_hour.unwrap());
+    debug!("total_hour({}): {:?}", &total_hour, &total_hour);
+    if 140. > total_hour.raw() {
+        error!("一人月の労働時間は140時間以上にしてください。");
+        return;
+    } else if 0. < total_hour.reminder() {
+        error!(
+            "{:.2}時間余分です。労働時間は15分刻みで入力してください。",
+            total_hour.reminder()
+        );
+        return;
+    }
 
-  // Work days
-  let raw_work_days = matches.value_of("work_days").unwrap();
-  let work_days = raw_work_days.parse();
-  if let Err(e) = work_days {
-    error!("労働日数が誤っています。");
-    error!("数字であることを確認してください: {}.", &raw_work_days);
-    panic!("ParseError: {}.", e);
-  }
-  let work_days = work_days.unwrap();
+    // Work days
+    let raw_work_days = matches.value_of("work_days").unwrap();
+    let work_days = raw_work_days.parse();
+    if let Err(e) = work_days {
+        error!("労働日数が誤っています。");
+        error!("数字であることを確認してください: {}.", &raw_work_days);
+        panic!("ParseError: {}.", e);
+    }
+    let work_days = work_days.unwrap();
 
-  if work_days > 31 {
-    error!("31日以上入力しないでください。");
-    panic!("work_days({}) exceeded 31.", work_days);
-  }
+    if work_days > 31 {
+        error!("31日以上入力しないでください。");
+        panic!("work_days({}) exceeded 31.", work_days);
+    }
 
+    // SBN_クラウドポータルv1.25開発
+    let portal_plan = plan::Plan::new(
+        "210915-01",
+        0.5,
+        total_hour,
+        work_days,
+        "SBN_クラウドポータルv1.25開発",
+    );
+    debug!("portal_plan: {:?}", &portal_plan);
 
-  // SBN_クラウドポータルv1.25開発
-  let portal_plan = plan::Plan::new(
-    "210915-01",
-    0.5,
-    total_hour,
-    work_days,
-    "SBN_クラウドポータルv1.25開発"
-  );
-  debug!("portal_plan: {:?}", &portal_plan);
+    // SBN_クラウドGW
+    let gw_cloud_plan = plan::Plan::new(
+        "206175-01",
+        0.2,
+        total_hour,
+        work_days,
+        "SBN_クラウドGW_v1.24開発",
+    );
+    debug!("gw_cloud_plan: {:?}", &gw_cloud_plan);
 
+    // SBNサービス運営
+    let service_plan = plan::Plan::new(
+        "191852-27",
+        0.3,
+        total_hour,
+        work_days,
+        "SBNサービス運営_2021年08月 / MC運用業務（業託）MC運用業務（業託） ※25日までに入力すること",
+    );
+    debug!("service_plan: {:?}", &service_plan);
 
-  // SBN_クラウドGW
-  let gw_cloud_plan = plan::Plan::new(
-    "206175-01",
-    0.2,
-    total_hour,
-    work_days,
-    "SBN_クラウドGW_v1.24開発"
-  );
-  debug!("gw_cloud_plan: {:?}", &gw_cloud_plan);
+    let plan_sum =
+        portal_plan.total_hour() + gw_cloud_plan.total_hour() + service_plan.total_hour();
+    if total_hour != plan_sum {
+        error!("計算結果と合計時間が異なる。");
+        panic!(
+            "total_hour({}) and plan_sum({}) are different",
+            total_hour, plan_sum
+        );
+    }
 
+    println!(
+        "一日の基本労働時間: {:.2} 時間",
+        (total_hour / work_days as f64).hour()
+    );
+    println!("{}", service_plan);
+    println!("{}", gw_cloud_plan);
+    println!("{}", portal_plan);
 
-  // SBNサービス運営
-  let service_plan = plan::Plan::new(
-    "191852-27",
-    0.3,
-    total_hour,
-    work_days,
-    "SBNサービス運営_2021年08月 / MC運用業務（業託）MC運用業務（業託） ※25日までに入力すること"
-  );
-  debug!("service_plan: {:?}", &service_plan);
+    println!();
 
+    let plan_work_days =
+        portal_plan.work_days() + gw_cloud_plan.work_days() + service_plan.work_days();
+    println!(
+        "プロジェクト間分割不可能日数 (各プロジェクトの余り時間を入力): {:1.0} 日",
+        work_days - plan_work_days
+    );
 
-  let plan_sum = portal_plan.total_hour() + gw_cloud_plan.total_hour() + service_plan.total_hour();
-  if total_hour != plan_sum {
-    error!("計算結果と合計時間が異なる。");
-    panic!("total_hour({}) and plan_sum({}) are different", total_hour, plan_sum);
-  }
-
-
-  println!("一日の基本労働時間: {:.2} 時間", (total_hour / work_days as f64).hour());
-  println!("{}", service_plan);
-  println!("{}", gw_cloud_plan);
-  println!("{}", portal_plan);
-
-  println!();
-
-  let plan_work_days = portal_plan.work_days() + gw_cloud_plan.work_days() + service_plan.work_days();
-  println!("プロジェクト間分割不可能日数 (各プロジェクトの余り時間を入力): {:1.0} 日", work_days - plan_work_days);
-
-  let plan_total_hour = portal_plan.total_hour().hour() + gw_cloud_plan.total_hour().hour() + service_plan.total_hour().hour();
-  println!("プロジェクト間分割不可能時間 (プロジェクト間分割不可能日に追加): {:.2} 時間", total_hour - plan_total_hour);
+    let plan_total_hour = portal_plan.total_hour().hour()
+        + gw_cloud_plan.total_hour().hour()
+        + service_plan.total_hour().hour();
+    println!(
+        "プロジェクト間分割不可能時間 (プロジェクト間分割不可能日に追加): {:.2} 時間",
+        total_hour - plan_total_hour
+    );
 }
--- src/work_hour.rs
+++ src/work_hour.rs
@@ -1,174 +1,176 @@
-use std::{fmt, cmp, ops};
+use std::{cmp, fmt, ops};
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct WorkHour {
-  value: i32,
-  reminder: f64,
+    value: i32,
+    reminder: f64,
 }
 
 impl WorkHour {
-  const QUARTER: f64 = 0.25;
-  pub fn new(hour: f64) -> Self {
-    let value: i32 = (hour / WorkHour::QUARTER) as i32;
-    let reminder: f64 = hour % WorkHour::QUARTER;
-    Self { value: value, reminder: reminder }
-  }
+    const QUARTER: f64 = 0.25;
+    pub fn new(hour: f64) -> Self {
+        let value: i32 = (hour / WorkHour::QUARTER) as i32;
+        let reminder: f64 = hour % WorkHour::QUARTER;
+        Self {
+            value: value,
+            reminder: reminder,
+        }
+    }
 
-  pub fn raw(&self) -> f64 {
-    self.value as f64 * WorkHour::QUARTER + self.reminder
-  }
+    pub fn raw(&self) -> f64 {
+        self.value as f64 * WorkHour::QUARTER + self.reminder
+    }
 
-  pub fn hour(&self) -> f64 {
-    self.value as f64 * WorkHour::QUARTER
-  }
+    pub fn hour(&self) -> f64 {
+        self.value as f64 * WorkHour::QUARTER
+    }
 
-  pub fn reminder(&self) -> f64 {
-    self.reminder
-  }
+    pub fn reminder(&self) -> f64 {
+        self.reminder
+    }
 }
 
 impl fmt::Display for WorkHour {
-  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-     write!(f, "{:.2}", self.hour())
-  }
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:.2}", self.hour())
+    }
 }
 
 impl cmp::PartialEq<f64> for WorkHour {
-  fn eq(&self, other: &f64) -> bool {
-    let other = WorkHour::new(*other);
-    self.value == other.value
-  }
+    fn eq(&self, other: &f64) -> bool {
+        let other = WorkHour::new(*other);
+        self.value == other.value
+    }
 }
 
 impl ops::Add for WorkHour {
-  type Output = Self;
-  fn add(self, other: Self) -> Self::Output {
-    Self::new(self.raw() + other.raw())
-  }
+    type Output = Self;
+    fn add(self, other: Self) -> Self::Output {
+        Self::new(self.raw() + other.raw())
+    }
 }
 
 impl ops::Add<f64> for WorkHour {
-  type Output = Self;
-  fn add(self, other: f64) -> Self::Output {
-    Self::new(self.raw() + other)
-  }
+    type Output = Self;
+    fn add(self, other: f64) -> Self::Output {
+        Self::new(self.raw() + other)
+    }
 }
 
 impl ops::Sub for WorkHour {
-  type Output = Self;
-  fn sub(self, other: Self) -> Self::Output {
-    Self::new(self.raw() - other.raw())
-  }
+    type Output = Self;
+    fn sub(self, other: Self) -> Self::Output {
+        Self::new(self.raw() - other.raw())
+    }
 }
 
 impl ops::Sub<f64> for WorkHour {
-  type Output = Self;
-  fn sub(self, other: f64) -> Self::Output {
-    Self::new(self.raw() - other)
-  }
+    type Output = Self;
+    fn sub(self, other: f64) -> Self::Output {
+        Self::new(self.raw() - other)
+    }
 }
 
 impl ops::Mul for WorkHour {
-  type Output = Self;
-  fn mul(self, other: Self) -> Self::Output {
-    Self::new(self.raw() * other.raw())
-  }
+    type Output = Self;
+    fn mul(self, other: Self) -> Self::Output {
+        Self::new(self.raw() * other.raw())
+    }
 }
 
 impl ops::Mul<f64> for WorkHour {
-  type Output = Self;
-  fn mul(self, other: f64) -> Self::Output {
-    Self::new(self.raw() * other)
-  }
+    type Output = Self;
+    fn mul(self, other: f64) -> Self::Output {
+        Self::new(self.raw() * other)
+    }
 }
 
 impl ops::Div for WorkHour {
-  type Output = Self;
-  fn div(self, other: Self) -> Self::Output {
-    Self::new(self.raw() / other.raw())
-  }
+    type Output = Self;
+    fn div(self, other: Self) -> Self::Output {
+        Self::new(self.raw() / other.raw())
+    }
 }
 
 impl ops::Div<f64> for WorkHour {
-  type Output = Self;
-  fn div(self, other: f64) -> Self::Output {
-    Self::new(self.raw() / other)
-  }
+    type Output = Self;
+    fn div(self, other: f64) -> Self::Output {
+        Self::new(self.raw() / other)
+    }
 }
 
 impl ops::Rem for WorkHour {
-  type Output = Self;
-  fn rem(self, other: Self) -> Self::Output {
-    Self::new(self.raw() % other.raw())
-  }
+    type Output = Self;
+    fn rem(self, other: Self) -> Self::Output {
+        Self::new(self.raw() % other.raw())
+    }
 }
 
 impl ops::Rem<f64> for WorkHour {
-  type Output = Self;
-  fn rem(self, other: f64) -> Self::Output {
-    Self::new(self.raw() % other)
-  }
+    type Output = Self;
+    fn rem(self, other: f64) -> Self::Output {
+        Self::new(self.raw() % other)
+    }
 }
-
 
 #[cfg(test)]
 mod tests {
-  #[test]
-  fn be_valid_value() {
-    let wh = super::WorkHour::new(170.50);
-    assert_eq!(wh, super::WorkHour::new(170.50));
-    assert_eq!(wh, 170.50);
-    assert_eq!(wh.to_string(), "170.50");
-  }
+    #[test]
+    fn be_valid_value() {
+        let wh = super::WorkHour::new(170.50);
+        assert_eq!(wh, super::WorkHour::new(170.50));
+        assert_eq!(wh, 170.50);
+        assert_eq!(wh.to_string(), "170.50");
+    }
 
-  #[test]
-  fn be_valid_add() {
-    let test1 = super::WorkHour::new(145.25) + super::WorkHour::new(0.25);
-    assert_eq!(test1, 145.5);
-    let test2 = super::WorkHour::new(145.25) + 0.25;
-    assert_eq!(test2, 145.5);
-  }
+    #[test]
+    fn be_valid_add() {
+        let test1 = super::WorkHour::new(145.25) + super::WorkHour::new(0.25);
+        assert_eq!(test1, 145.5);
+        let test2 = super::WorkHour::new(145.25) + 0.25;
+        assert_eq!(test2, 145.5);
+    }
 
-  #[test]
-  fn be_valid_minus() {
-    let test1 = super::WorkHour::new(145.25) - super::WorkHour::new(1.25);
-    assert_eq!(test1, 144.0);
-    let test2 = super::WorkHour::new(145.25) - 2.50;
-    assert_eq!(test2, 142.75);
-  }
+    #[test]
+    fn be_valid_minus() {
+        let test1 = super::WorkHour::new(145.25) - super::WorkHour::new(1.25);
+        assert_eq!(test1, 144.0);
+        let test2 = super::WorkHour::new(145.25) - 2.50;
+        assert_eq!(test2, 142.75);
+    }
 
-  #[test]
-  fn be_valid_times() {
-    let test1 = super::WorkHour::new(145.25) * super::WorkHour::new(1.25);
-    assert_eq!(test1, 181.5);
-    assert_eq!(test1.reminder(), 0.0625);
-    let test2 = super::WorkHour::new(145.25) * 2.50;
-    assert_eq!(test2, 363.0);
-    assert_eq!(test2.reminder(), 0.125);
-    let test3 = super::WorkHour::new(145.25) * 0.50;
-    assert_eq!(test3, 72.5);
-    assert_eq!(test3.reminder(), 0.125);
-  }
+    #[test]
+    fn be_valid_times() {
+        let test1 = super::WorkHour::new(145.25) * super::WorkHour::new(1.25);
+        assert_eq!(test1, 181.5);
+        assert_eq!(test1.reminder(), 0.0625);
+        let test2 = super::WorkHour::new(145.25) * 2.50;
+        assert_eq!(test2, 363.0);
+        assert_eq!(test2.reminder(), 0.125);
+        let test3 = super::WorkHour::new(145.25) * 0.50;
+        assert_eq!(test3, 72.5);
+        assert_eq!(test3.reminder(), 0.125);
+    }
 
-  #[test]
-  fn be_valid_div() {
-    let test1 = super::WorkHour::new(145.25) / super::WorkHour::new(1.25);
-    assert_eq!(test1, 116.0);
-    let test2 = super::WorkHour::new(145.25) / 2.50;
-    assert_eq!(test2, 58.0);
-    let test3 = super::WorkHour::new(145.25) / 0.30;
-    assert_eq!(test3, 484.0);
-  }
+    #[test]
+    fn be_valid_div() {
+        let test1 = super::WorkHour::new(145.25) / super::WorkHour::new(1.25);
+        assert_eq!(test1, 116.0);
+        let test2 = super::WorkHour::new(145.25) / 2.50;
+        assert_eq!(test2, 58.0);
+        let test3 = super::WorkHour::new(145.25) / 0.30;
+        assert_eq!(test3, 484.0);
+    }
 
-  #[test]
-  fn be_valid_rem() {
-    let test1 = super::WorkHour::new(145.25) % super::WorkHour::new(1.25);
-    assert_eq!(test1, 0.25);
-    let test2 = super::WorkHour::new(145.25) % 2.50;
-    assert_eq!(test2, 0.25);
-    let test3 = super::WorkHour::new(145.25) % 0.30;
-    assert_eq!(test3, 0.0);
-    let test4 = super::WorkHour::new(145.25) % 150.0;
-    assert_eq!(test4, 145.25);
-  }
+    #[test]
+    fn be_valid_rem() {
+        let test1 = super::WorkHour::new(145.25) % super::WorkHour::new(1.25);
+        assert_eq!(test1, 0.25);
+        let test2 = super::WorkHour::new(145.25) % 2.50;
+        assert_eq!(test2, 0.25);
+        let test3 = super::WorkHour::new(145.25) % 0.30;
+        assert_eq!(test3, 0.0);
+        let test4 = super::WorkHour::new(145.25) % 150.0;
+        assert_eq!(test4, 145.25);
+    }
 }
