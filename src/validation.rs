942cbd1d4f9130aad56a1fc46c9b9fb824bb374f
Toshiki.Kawazoe <toshiki.kawazoe@sony.com>
2021-09-02 22:28:22 +0900

    Fix variable 'feature/validation'

--- src/validation.rs
+++ src/validation.rs
@@ -26,13 +26,13 @@ pub fn valid_equals_total_hour_and_plans_total_hour(
     total_hour: &WorkHour,
     plans: &Vec<plan::Plan>,
 ) -> Result<(), Box<dyn Error>> {
-    let plan_sum = plans
+    let plan_total_hour = plans
         .iter()
         .fold(WorkHour::new(0.0), |sum, p| sum + p.total_hour());
-    if *total_hour == plan_sum {
+    if *total_hour == plan_total_hour {
         Ok(())
     } else {
-        error!("計算結果と合計時間が不一致: {} != {}", total_hour, plan_sum);
+        error!("計算結果と合計時間が不一致: {} != {}", total_hour, plan_total_hour);
         Err(Box::new(
             ValidError::NotEqualsTotalHourAndPlansTotalHourError,
         ))
